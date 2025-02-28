9dc5717cee3be8c353c60938336858576db06cb9
ranof11 <rajeshtriadi14@gmail.com>
2025-02-28 11:08:48 +0700

    Initial Commit

---
+++ SpeechTest/SpeechTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechTestApp.swift
+//  SpeechTest
+//
+//  Created by Rajesh Triadi Noftarizal on 28/02/25.
+//
+
+import SwiftUI
+
+@main
+struct SpeechTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
